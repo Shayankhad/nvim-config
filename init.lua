f8f39f3ee1d2ffb2ab2f573b3a3fe4dbaae20757
ShayanKhad <shayan.khademi7812@gmail.com>
2026-07-15 11:29:54 +0330

    add key for cpp comment remove

--- init.lua
+++ init.lua
@@ -62,7 +62,7 @@ vim.keymap.set('n', '<C-Up>', ':resize -2<CR>', { desc = 'Decrease height' })
 vim.keymap.set('n', '<C-Down>', ':resize +2<CR>', { desc = 'Increase height' })
 vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', { desc = 'Decrease width' })
 vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', { desc = 'Increase width' })
-
+vim.keymap.set('n', '<leader>rc', ':%s/\\/\\/.*\\|\\/\\*\\_.\\{-}\\*\\///ge<CR>:noh<CR>', { desc = 'Remove C++ comments' })
 require("lazy").setup({
 
   {
