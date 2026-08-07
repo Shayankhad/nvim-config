28f76a0b66da927ad0851a1417951a68df6afd97
ShayanKhad <shayan.khademi7812@gmail.com>
2026-08-08 00:41:44 +0330

    Added LSP

--- init.lua
+++ init.lua
@@ -281,6 +281,34 @@ require("lazy").setup({
   {
     "stevearc/conform.nvim",
     opts = {},
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {},
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {},
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+    end,
+  },
+  {
+    "saghen/blink.cmp",
+    version = "*",
+    dependencies = "rafamadriz/friendly-snippets",
+    opts = {
+      keymap = { preset = "default" },
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = "mono",
+      },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+    },
   }
 },
 {
