b51e0594a472b02810e6faf0cf6630b1109ad622
ShayanKhad <shayan.khademi7812@gmail.com>
2026-08-07 11:31:03 +0330

    added telescope

--- init.lua
+++ init.lua
@@ -204,6 +204,38 @@ require("lazy").setup({
         })
       end,
     },
+{
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.8",                              -- or "latest" for stable
+  dependencies = {
+    "nvim-lua/plenary.nvim",                  -- required
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" }, -- optional, faster fuzzy matching
+  },
+  config = function()
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
+
+    telescope.setup({
+      defaults = {
+        preview = {
+          treesitter = false,   -- <-- add this
+        },
+        mappings = {
+          i = {
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
+          },
+        },
+      },
+    })
+
+    pcall(telescope.load_extension, "fzf")
+
+    local builtin = require("telescope.builtin")
+    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+    vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+  end,
+  }
 },
 {
   install = { colorscheme = { "onedark" } },
