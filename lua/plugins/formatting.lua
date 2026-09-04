78e60ba87295107b8cfd65b84b158d254abde2cc
ShayanKhad <shayan.khademi7812@gmail.com>
2026-09-04 18:37:00 +0330

    added couple LPS and formmatter and linters

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,6 +1,34 @@
 return {
   {
     "stevearc/conform.nvim",
-    opts = {},
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "ruff_format" },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        json = { "prettier" },
+        c = { "clang-format" },
+        cpp = { "clang-format" },
+        sh = { "shfmt" },
+        bash = { "shfmt" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+    },
+    keys = {
+      {
+        "<leader>f",
+        function()
+          require("conform").format({ async = true, lsp_fallback = true })
+        end,
+        mode = { "n", "v" },
+        desc = "Format buffer",
+      },
+    },
   },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,23 +6,71 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     opts = {
-      ensure_installed = { "pyright" },
+      ensure_installed = {
+        "pyright",
+        "ts_ls",
+        "lua_ls",
+        "bashls",
+        "clangd",
+        "jsonls",
+      },
+    },
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "prettier",
+        "ruff",
+        "clang-format",
+        "shfmt",
+        "shellcheck",
+      },
     },
   },
   {
     "neovim/nvim-lspconfig",
     dependencies = { "saghen/blink.cmp" },
     config = function()
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
-      vim.lsp.config('pyright', {
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
+
+      -- Global default capabilities for all LSP servers
+      vim.lsp.config("*", {
         capabilities = capabilities,
-        settings = {
-          python = {
-            pythonPath = vim.fn.expand("~/ml/venv/bin/python")
-          }
-        }
       })
-      vim.lsp.enable('pyright')
+
+      -- Pyright: use custom venv if present, otherwise rely on active environment
+      local venv_python = vim.fn.expand("~/ml/venv/bin/python")
+      local pyright_settings = {}
+      if vim.fn.filereadable(venv_python) == 1 then
+        pyright_settings = {
+          python = {
+            pythonPath = venv_python,
+          },
+        }
+      end
+      vim.lsp.config("pyright", {
+        settings = pyright_settings,
+      })
+
+      -- Lua language server: recognize global 'vim'
+      vim.lsp.config("lua_ls", {
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+          },
+        },
+      })
+
+      -- Enable all servers
+      local servers = { "pyright", "ts_ls", "lua_ls", "bashls", "clangd", "jsonls" }
+      for _, server in ipairs(servers) do
+        vim.lsp.enable(server)
+      end
     end,
   },
   {
